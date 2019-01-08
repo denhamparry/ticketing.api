ff4aff3730d57302c4de3ce0577f7829cc09403f
Lewis Denham-Parry <lewis@denhamparry.co.uk>
2019-01-08 10:59:28 +0000

    Setup https

--- api/Startup.cs
+++ api/Startup.cs
@@ -43,8 +43,7 @@ namespace Ticketing.API
             {
                 app.UseHsts();
             }
-
-            // app.UseHttpsRedirection();
+            app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
