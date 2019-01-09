0851786b320df7917fa1f2a193de3b505b2b94c6
Lewis Denham-Parry <lewis@denhamparry.co.uk>
2019-01-09 16:20:14 +0000

    RabbitMQ init

--- api/README.md
+++ api/README.md
@@ -12,3 +12,28 @@ Writing...
 ## References
 
 * [ASP.NET Web API and MongoDB](https://docs.microsoft.com/en-us/aspnet/core/tutorials/first-mongo-app?view=aspnetcore-2.2&tabs=visual-studio-code)
+
+## RabbitMQ
+
+### Setup RabbitMQ
+
+#### Local
+
+Install RabbitMQ locally:
+
+```bash
+$ brew update
+Updating Homebrew...
+$ brew install rabbitmq
+Updating Homebrew...
+$ brew upgrade rabbitmq
+Updating Homebrew...
+$ export PATH=$PATH:/usr/local/sbin
+...
+```
+
+#### Docker
+
+'''bash
+$ docker run -d --name ticketing.messaging rabbitmq:3-management
+'''
--- api/Startup.cs
+++ api/Startup.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using MassTransit;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,6 +13,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using RabbitMQ.Client;
 using Ticketing.API.Services;
 
 namespace Ticketing.API
@@ -21,15 +25,41 @@ namespace Ticketing.API
             Configuration = configuration;
         }
 
+        public IContainer ApplicationContainer { get; private set; }
+
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public IServiceProvider ConfigureServices(IServiceCollection services)
         {
             services.Configure<AppConfiguration>(Configuration.GetSection("AppConfiguration"));
             services.Configure<AppConfiguration>(Configuration);
             services.AddScoped<TicketService>();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            var builder = new ContainerBuilder();
+            builder.Register(c =>
+                {
+                    return Bus.Factory.CreateUsingRabbitMq(sbc =>
+                    {
+                        sbc.Host(Configuration.GetConnectionString("Messaging"), "/", h =>
+                        {
+                            h.Username("guest");
+                            h.Password("guest");
+                        });
+
+                        sbc.ExchangeType = ExchangeType.Fanout;
+                    });
+                })
+                .As<IBusControl>()
+                .As<IBus>()
+                .As<IPublishEndpoint>()
+                .SingleInstance();
+
+            builder.Populate(services);
+            ApplicationContainer = builder.Build();
+
+            return new AutofacServiceProvider(ApplicationContainer);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
