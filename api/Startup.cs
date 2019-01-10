ed8544f14a50d5b4d95ca9e94b429754c4d865ff
Lewis Denham-Parry <lewis@denhamparry.co.uk>
2019-01-10 11:36:44 +0000

    Remove injection

--- api/Startup.cs
+++ api/Startup.cs
@@ -2,9 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Autofac;
-using Autofac.Extensions.DependencyInjection;
-using MassTransit;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -13,7 +10,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using RabbitMQ.Client;
 using Ticketing.API.Services;
 
 namespace Ticketing.API
@@ -25,41 +21,15 @@ namespace Ticketing.API
             Configuration = configuration;
         }
 
-        public IContainer ApplicationContainer { get; private set; }
-
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public IServiceProvider ConfigureServices(IServiceCollection services)
+        public void ConfigureServices(IServiceCollection services)
         {
             services.Configure<AppConfiguration>(Configuration.GetSection("AppConfiguration"));
             services.Configure<AppConfiguration>(Configuration);
             services.AddScoped<TicketService>();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-
-            var builder = new ContainerBuilder();
-            builder.Register(c =>
-                {
-                    return Bus.Factory.CreateUsingRabbitMq(sbc =>
-                    {
-                        sbc.Host(Configuration.GetConnectionString("Messaging"), "/", h =>
-                        {
-                            h.Username("guest");
-                            h.Password("guest");
-                        });
-
-                        sbc.ExchangeType = ExchangeType.Fanout;
-                    });
-                })
-                .As<IBusControl>()
-                .As<IBus>()
-                .As<IPublishEndpoint>()
-                .SingleInstance();
-
-            builder.Populate(services);
-            ApplicationContainer = builder.Build();
-
-            return new AutofacServiceProvider(ApplicationContainer);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
